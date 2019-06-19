d05f2dce60fc86f05ad9005adff51c63a5214574
Elijah Luckey <luckeyelijah112@gmail.com>
2019-06-19 11:04:35 -0400

    Added edge case where the user may be Poly_Secure.

--- scripts/vpn_connect.sh
+++ scripts/vpn_connect.sh
@@ -3,6 +3,8 @@
 upStatus='Connected'
 downStatus='Disconnected'
 curStatus=`globalprotect show --status | awk '{ print $4 }'`
+curNet=`nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2`
+polySecNet='Poly_Secure'
 
 function disconnectVPN(){
      globalprotect disconnect
@@ -12,21 +14,35 @@ function connectVPN(){
      globalprotect connect -p vpn.floridapoly.edu
 }
 
-case "$curStatus" in
-     
-     "$upStatus")
-          echo "Disconnecting VPN."
-          disconnectVPN
-          exit 0
-          ;;
-     
-     "$downStatus")
-          echo "Connecting VPN."
-          connectVPN
-          exit 0
-          ;;
-     *)
-          echo "Cannot determine status. Exiting."
-          exit 1
-          ;;
-esac
+function onPolySecure(){
+     if [ "$curNet" == "yes:Poly_Secure" ]
+     then
+          echo "You're on the $polySecNet network. No need to connect to VPN."
+     else
+          # echo "You're not on the $polySecNet network."
+          connectCase
+     fi
+}
+
+function connectCase(){
+     case "$curStatus" in
+          
+          "$upStatus")
+               echo "Disconnecting VPN."
+               disconnectVPN
+               exit 0
+               ;;
+          
+          "$downStatus")
+               echo "Connecting VPN."
+               connectVPN
+               exit 0
+               ;;
+          *)
+               echo "Cannot determine status. Exiting."
+               exit 1
+               ;;
+     esac
+}
+
+onPolySecure
