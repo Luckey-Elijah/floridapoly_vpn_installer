56772d6e9b77cb6b432e2a7c92b43ade714da9fb
Elijah Luckey <luckeyelijah112@gmail.com>
2019-06-18 20:24:51 -0400

    toggles the connected/disconnected state of vpn

--- scripts/vpn_connect.sh
+++ scripts/vpn_connect.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
-# Quick connect to Florida Poly VPN via globalprotect
-globalprotect connect -p vpn.floridapoly.edu
+upStatus='Connected'
+downStatus='Disconnected'
+curStatus=`globalprotect show --status | awk '{ print $4 }'`
+
+function disconnectVPN(){
+     globalprotect disconnect
+}
+
+function connectVPN(){
+     globalprotect connect -p vpn.floridapoly.edu
+}
+
+case "$curStatus" in
+     
+     "$upStatus")
+          echo "Disconnecting VPN."
+          disconnectVPN
+          exit 0
+          ;;
+     
+     "$downStatus")
+          echo "Connecting VPN."
+          connectVPN
+          exit 0
+          ;;
+     *)
+          echo "Cannot determine status. Exiting."
+          exit 1
+          ;;
+esac
